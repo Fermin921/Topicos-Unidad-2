ac21a6d22899ffecd55753350ece26216a1e6f5c
fer_m <fer_m@DESKTOP-IBFI0VS>
2021-10-09 10:58:09 -0500

    Serie de numeros

---
+++ src/Ejercicio/VentanaSerie.java
@@ -0,0 +1,175 @@
+package Ejercicio;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+
+import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+public class VentanaSerie extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtNumero1;
+	private JTextField txtNumero2;
+	private JTextField txtCantCiclo;
+	private JTextField txtMostrar;
+	private JButton btnSalir;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaSerie frame = new VentanaSerie();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaSerie() {
+		setTitle("19100209 Victor Fermin Martinez Alvarado");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 575, 340);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Ingrese el primer numero:");
+		
+		JLabel lblNewLabel_1 = new JLabel("Ingrese el segundo numero:");
+		
+		JLabel lblNewLabel_2 = new JLabel("Ingrese la cantidad de veces que se repetira:");
+		
+		txtNumero1 = new JTextField();
+		txtNumero1.setColumns(10);
+		
+		txtNumero2 = new JTextField();
+		txtNumero2.setColumns(10);
+		
+		txtCantCiclo = new JTextField();
+		txtCantCiclo.setColumns(10);
+		
+		JButton btnGenerar = new JButton("Generar");
+		btnGenerar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Numero1 = txtNumero1.getText();
+				String Numero2 = txtNumero2.getText();
+				int Iterador = Integer.parseInt(txtCantCiclo.getText());
+				int Contador = 0;
+				int Suma = 0;
+				String Cadena = "";
+			    Cadena = Numero1 + " " + Numero2 ;
+			   
+			    do
+			    {
+			    	
+			    	Contador = Contador + 1;
+			    	Suma = Integer.parseInt(Numero2) + Integer.parseInt(Numero1);
+			    	Cadena += " " + String.valueOf(Suma);
+			    	txtMostrar.setText (Cadena);
+			    	Numero1 = Integer.toString(Integer.parseInt(Numero2));
+			    	Numero2 = Integer.toString(Suma);
+			    	
+			    }
+			    while (Contador <= Iterador);
+			    
+			}
+		});
+		btnGenerar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		txtMostrar = new JTextField();
+		txtMostrar.setColumns(10);
+		
+		btnSalir = new JButton("Salir");
+		btnSalir.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(26)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(lblNewLabel_1)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(txtNumero2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(lblNewLabel)
+									.addGap(18)
+									.addComponent(txtNumero1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addGap(91)
+									.addComponent(lblNewLabel_3))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(lblNewLabel_2)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(txtCantCiclo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+									.addComponent(btnSalir)
+									.addComponent(txtMostrar, GroupLayout.PREFERRED_SIZE, 298, GroupLayout.PREFERRED_SIZE))))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(36)
+							.addComponent(btnGenerar)))
+					.addContainerGap(158, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(29)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel)
+						.addComponent(txtNumero1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_3))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_1)
+						.addComponent(txtNumero2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_2)
+						.addComponent(txtCantCiclo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(txtMostrar, GroupLayout.DEFAULT_SIZE, 76, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnGenerar)
+						.addComponent(btnSalir))
+					.addGap(52))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
